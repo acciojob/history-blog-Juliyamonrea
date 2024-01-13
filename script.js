af02aa005215b061c9d1d8e8eaa5fa5a73c7376b
Juliyamonrea <134256875+Juliyamonrea@users.noreply.github.com>
2024-01-13 17:52:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,1 @@
-//your JS code here. If required.
-cy.get('img').should('be.visible');
-cy.wait(2000); // Adjust the wait time based on your application's behavior
-// Adjust the number '2' to match the expected number of elements
-cy.get('your-selector').should('have.length', 2);
+
